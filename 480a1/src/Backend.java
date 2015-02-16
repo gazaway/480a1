8907cb6316a01f67b4505622bd85fc87bebb72aa
gazaway <j_i_m_m_y_g@hotmail.com>
2015-02-16 16:54:11 -0700

    Second commit. Test

--- 480a1/src/Backend.java
+++ 480a1/src/Backend.java
@@ -1,7 +1,10 @@
 public class Backend {
 
 	public static void main(String[] args) {
-
+		/*
+		 * In order to do this project I basically have to create tuples of
+		 * all of the word and word pairs in the corpus and output accordingly.
+		 */
 	}
 
 }
